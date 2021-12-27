ad782a12493e964de4003f7d6fb2b278921d6a0f
Tony Francis <tonyfrancisv@gmail.com>
2021-12-27 10:48:20 -0800

    expose fills api

--- aioftx/options/api.py
+++ aioftx/options/api.py
@@ -1,4 +1,5 @@
 from .account.api import *
+from .fills.api import *
 from .positions.api import *
 from .quote_request.api import *
 from .quotes.api import *
--- aioftx/options/schemas.py
+++ aioftx/options/schemas.py
@@ -1,4 +1,5 @@
 from .account.schemas import *
+from .fills.schemas import *
 from .positions.schemas import *
 from .quote_request.schemas import *
 from .quotes.schemas import *
